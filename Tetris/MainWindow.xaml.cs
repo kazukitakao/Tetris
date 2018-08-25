18d1a896fd1b95a6aeb681a89a09edf2863f7ab1
win-kt <kazukitakao55@gmail.com>
2018-08-26 04:01:43 +0900

    テトリミノの設定クラスを作成

--- Tetris/MainWindow.xaml.cs
+++ Tetris/MainWindow.xaml.cs
@@ -15,6 +15,120 @@ using System.Windows.Shapes;
 
 namespace Tetris
 {
+    public class Tetramino
+    {
+        private Point currentPosition;
+        private Point[] currentShape;
+        private Brush currentColor;
+        private Boolean rotate;
+
+        public Tetramino()
+        {
+            currentPosition = new Point(0, 0);
+            currentColor = Brushes.Transparent;
+            currentShape = setRandomShape();
+        }
+
+        // TODO:プロパティに置き換え
+        public Brush GetCurrentColor()
+        {
+            return currentColor;
+        }
+
+        public Point getCurrentPosition()
+        {
+            return currentPosition;
+        }
+
+        public Point[] GetCurrentShape()
+        {
+            return currentShape;
+        }
+
+        private Point[] setRandomShape()
+        {
+            Random rand = new Random();
+            switch (rand.Next() % 7)
+            {
+                case 0: // I
+                    rotate = true;
+                    currentColor = Brushes.Cyan;
+                    return new Point[] 
+                    {
+                        new Point(0,0),
+                        new Point(-1,0),
+                        new Point(1,0),
+                        new Point(2,0)
+                    };
+                case 1:// J
+                    rotate = true;
+                    currentColor = Brushes.Blue;
+                    return new Point[]
+                    {
+                        new Point(1,-1),
+                        new Point(=1,0),
+                        new Point(0,0),
+                        new Point(0,0)
+                    };
+                case 2:// L
+                    rotate = true;
+                    currentColor = Brushes.Orange;
+                    return new Point[]
+                    {
+                        new Point(0,0),
+                        new Point(-1,0),
+                        new Point(1,0),
+                        new Point(1,-1)
+                    };
+                case 3:// O
+                    rotate = true;
+                    currentColor = Brushes.Yellow;
+                    return new Point[]
+                    {
+                        new Point(0,0),
+                        new Point(0,1),
+                        new Point(1,0),
+                        new Point(1,1)
+                    };
+                case 4:// S
+                    rotate = true;
+                    currentColor = Brushes.Green;
+                    return new Point[]
+                    {
+                        new Point(0,0),
+                        new Point(-1,0),
+                        new Point(0,-1),
+                        new Point(1,0)
+                    };
+                case 5:// T
+                    rotate = true;
+                    currentColor = Brushes.Purple;
+                    return new Point[]
+                    {
+                        new Point(0,0),
+                        new Point(-1,0),
+                        new Point(0,-1),
+                        new Point(1,1)
+                    };
+                case 6:// Z
+                    rotate = true;
+                    currentColor = Brushes.Red;
+                    return new Point[]
+                    {
+                        new Point(0,0),
+                        new Point(-1,0),
+                        new Point(0,1),
+                        new Point(0,1),
+                        new Point(1,1)
+                    };
+                default:
+                    return null;
+            }
+        }
+
+    }
+
+
     /// <summary>
     /// MainWindow.xaml の相互作用ロジック
     /// </summary>
